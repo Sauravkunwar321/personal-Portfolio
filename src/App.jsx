868facc863fdb9441b4e577fd461347def13ba87
Saurav Kunwar <sauravkunwar63@gmail.com>
2025-02-09 23:26:46 +0545

    Corrected code

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SocialLinks from './components/SocialLinks'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
 import Experience from './components/Experience'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 
 
 function App() {
