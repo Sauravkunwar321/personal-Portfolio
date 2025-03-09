989ab51684602d3846e5606171be5b9ed7a73b58
Saurav Kunwar <sauravkunwar63@gmail.com>
2025-03-10 00:27:23 +0545

    added new ones

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -52,19 +52,13 @@ const portfolios = [
         
         
     },
-    {
-        id:7,
-        title:"Leetmetrics",
-        src:Leetmetrics,
-        link:`https://github.com/Sauravkunwar321/LeetMetrics`,
-        demo:`https://fluffy-alfajores-e17e69.netlify.app/`
-    },
+  
 ]
 
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen "
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
