16f14cb2c3616f20c4d817189684d61571d6a7fc
Saurav Kunwar <sauravkunwar63@gmail.com>
2025-04-07 22:30:52 +0545

    adjusted the alignment

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const contact = () => {
   return (
-    <div name="contact" className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white ">
+    <div name="contact" className="w-full h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white pt-70">
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-bottom-4 border-gray-500">Contact</p>
