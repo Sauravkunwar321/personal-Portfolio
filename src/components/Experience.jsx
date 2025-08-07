3731e2bfe581bfd1c1cecafab3ebb6ce03f1432f
sauravKunwar-DataInsight <sauravkunwar63@gmail.com>
2025-08-07 11:27:08 +0545

    updated padding in About, Experience, and Portfolio components for improved layout

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -71,7 +71,7 @@ const techs = [
 
 
   return (
-    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-full pt-90 '>
+    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-full pt-24 md:pt-90 '>
         <div className='max-w-screen-lg mx-auto p-4  flex flex-col justify-center w-full h-full text-white'>
             <div>
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline '>Experience</p>
